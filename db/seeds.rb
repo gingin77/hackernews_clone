65cdd8f6125e8ca229423a10a86c484c89e306bc
gingin77 <ginniehench@gmail.com>
2018-01-26 17:30:49 -0500

    Remove all comments from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-  seed_user = User.create({
-    name: 'seed_user',
-    email: 'test_user@email.com',
-    password: 'secret!'
-  })
-#   Character.create(name: 'Luke', movie: movies.first)
+seed_user = User.create({
+  name: 'seed_user',
+  email: 'test_user@email.com',
+  password: 'secret!'
+})
