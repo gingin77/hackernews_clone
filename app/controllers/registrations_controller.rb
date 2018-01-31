a0f0961417f226d8d17217394db8055102c5291a
gingin77 <ginniehench@gmail.com>
2018-01-31 10:47:27 -0500

    Refactor code for HN logo, nav view, registrations view and controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(user, params)
-    resource.update_without_password(params)
+    user.update_without_password(params)
   end
 end
