c8ec589fcf203982e2370652f320f2eaf5b03056
gingin77 <ginniehench@gmail.com>
2018-02-01 15:27:06 -0500

    WIP - Build some tests and validation code in the model

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,3 +1,23 @@
 class Submission < ApplicationRecord
-  belongs_to :user
+  # belongs_to :user
+
+  validates :title, presence: true, if: ->(submission){submission.url.present?}
+  validates :url, presence: true, if: ->(submission){submission.title.present?}
+
+  # validates_numericality_of :title, :url, allow_nil: true
+  # validates_numericality_of :text, allow_nil: true
+  #
+  # validate :title_and_url_together
+  # validate :text_or_title_and_url
+
+  private
+
+    # def title_and_url_together
+    #   if !title.blank?
+    #
+    #   end
+    # end
+    #
+    # def text_or_title_and_url
+    #   unless
 end
--- test/models/submission_test.rb
+++ test/models/submission_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SubmissionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @sub_tit_url = submissions(:one)
+    @sub_text = submissions(:two)
+    @sub_tit_text = submissions(:three)
+    @sub_tit = submissions(:four)
+    @sub_url = submissions(:five)
+  end
+
+  test "submission with url and title is valid" do
+    assert @sub_tit_url.valid?
+  end
+
+  test "submission with text is valid" do
+    assert @sub_text.valid?
+  end
+
+  test "submission with title and text is invalid" do
+    assert_not @sub_tit_text.valid?
+  end
+
+  test "submission with title only invalid" do
+    assert_not @sub_tit_text.valid?
+  end
+
+  test "submission with url only invalid" do
+    assert_not @sub_url.valid?
+  end
 end
