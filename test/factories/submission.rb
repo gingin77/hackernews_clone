be1db9d5ac9883f1fc3ff8667925c9e997a3c747
gingin77 <ginniehench@gmail.com>
2018-02-04 09:30:55 -0500

    In sub test - use build(:user) instead of create

--- test/factories/submission.rb
+++ test/factories/submission.rb
@@ -1,17 +1,17 @@
 FactoryBot.define do
   factory :user do
     name "Oliver Sacks"
-    email "olivers@email.com"
+    sequence(:email) { |n| "olivers#{n}@email.com" }
     password "secret"
   end
 
   factory :text_submission, class: Submission do
-    user { create(:user) }
+    user { build(:user) }
     text "Plants don’t get enough credit. They move. You know this. Your houseplant salutes the sun each morning. At night, it returns to center."
   end
 
   factory :url_submission, class: Submission do
-    user { create(:user) }
+    user { build(:user) }
     url "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience"
     title "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?"
   end
