e204937d8b27668fe49cd980167fca4e25112707
gingin77 <ginniehench@gmail.com>
2018-02-04 12:52:59 -0500

    Add view helper methods to user#show to improve UX; remove extra parantheses in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   private
 
   def user
-    @user ||= User.find_by(name: (params[:id]))
+    @user ||= User.find_by(name: params[:id])
   end
   helper_method :user
 
