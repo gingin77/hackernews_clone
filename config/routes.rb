da290212d868331e0a25f10944593bceb1fddb1c
gingin77 <ginniehench@gmail.com>
2018-02-05 12:34:17 -0500

    Change references to user instead of profile, after rebasing user_model branch into this submissions feature branch

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root to: "submissions#index"
 
   devise_for :users, controllers: { registrations: 'registrations' }
-
   resources :users, only: :show
+
   resources :submissions do
     post 'create_comment'
   end
