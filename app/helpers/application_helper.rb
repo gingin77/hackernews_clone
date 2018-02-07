a20e08f85cb6b431a8eeaba1163e82c685f9211e
gingin77 <ginniehench@gmail.com>
2018-02-07 11:27:39 -0500

    Refactor error message displays - now 2 helpers - one for base; one for inline-field specific messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
-  def show_errors(object, field_name)
-    if object.errors.any?
-      if !object.errors.messages[field_name].blank?
-        object.errors.messages[field_name].join(", ")
-      end
+  def show_errors_on_base(object)
+    if object.errors.any? && object.errors.messages[:base].present?
+      content_tag(:div, object.errors.full_messages_for(:base).join(", "), class: "alert alert-alert")
+    end
+  end
+
+  def show_errors_inline(object, field_name)
+    if object.errors.any? && object.errors.full_messages_for(field_name).present? && !new_post.errors[:base].present?
+      content_tag(:div, object.errors.full_messages_for(field_name).join(", "), class: "alert alert-alert inline-alert")
     end
   end
 end
