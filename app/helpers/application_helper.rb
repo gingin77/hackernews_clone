5a3b01552fb5472bc46a696990c4af9b4ce2afc5
gingin77 <ginniehench@gmail.com>
2018-02-07 23:57:27 -0500

    Minor styling updates related to form layout and error display on forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def show_errors_on_base(object)
-    if object.errors.any? && object.errors.messages[:base].present?
-      content_tag(:div, object.errors.full_messages_for(:base).join(", "), class: "alert alert-alert")
+  def show_errors_on_base(obj)
+    if obj.errors.any? && obj.errors.messages[:base].present?
+      content_tag(:div, obj.errors.full_messages_for(:base).join(", "), class: "alert alert-alert")
     end
   end
 
-  def show_errors_inline(object, field_name)
-    if object.errors.any? && object.errors.full_messages_for(field_name).present? && !new_post.errors[:base].present?
-      content_tag(:div, object.errors.full_messages_for(field_name).join(", "), class: "alert alert-alert inline-alert")
+  def show_errors_inline(obj, field_name)
+    if obj.errors.any? && obj.errors.full_messages_for(field_name).present? && !obj.errors[:base].present?
+      content_tag(:div, obj.errors.full_messages_for(field_name).join(", "), class: "alert alert-alert inline-alert")
     end
   end
 end
