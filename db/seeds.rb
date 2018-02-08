b8db5016c580125cfb8ea8a1cb77ac64592d87b3
gingin77 <ginniehench@gmail.com>
2018-02-08 13:48:54 -0500

    Modify seeds file so that Submissions will be created if users are present in AR

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,22 +9,39 @@ if User.first.nil?
     email: 'ellie@awesomedogs.com',
     password: 'secret!'
   })
-end
-
-posts = Submission.create([
-  {
-    user: user1,
-    text: "Plants don’t get enough credit. They move. You know this. Your houseplant salutes the sun each morning. At night, it returns to center."
-  },
-  {
-    user: user2,
-    url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience",
-    title: "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?"
-  }
-])
-
-Submission.create({
+  posts = Submission.create([
+    {
+      user: user1,
+      text: "Plants don’t get enough credit. They move. You know this. Your houseplant salutes the sun each morning. At night, it returns to center."
+    },
+    {
+      user: user2,
+      url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience",
+      title: "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?"
+    }
+  ])
+  Submission.create({
     user: user1,
     post: posts[0],
     text: "Yay for plants..."
-})
+  })
+elsif (!User.first.nil? && !User.last.nil?)
+  ar_user1 = User.first
+  ar_user2 = User.last
+  posts = Submission.create([
+    {
+      user: ar_user1,
+      text: "Plants don’t get enough credit. They move. You know this. Your houseplant salutes the sun each morning. At night, it returns to center."
+    },
+    {
+      user: ar_user2,
+      url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience",
+      title: "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?"
+    }
+  ])
+  Submission.create({
+    user: ar_user1,
+    post: posts[0],
+    text: "Yay for plants..."
+  })
+end
