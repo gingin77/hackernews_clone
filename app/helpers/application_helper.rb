dc7cc3ab4bbd3a3f56d3f0d82873a171ebb032d8
gingin77 <ginniehench@gmail.com>
2018-02-08 12:15:19 -0500

    Rename 'obj' as 'resource'

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def show_errors_on_base(obj)
-    if obj.errors.any? && obj.errors.messages[:base].present?
-      content_tag(:div, obj.errors.full_messages_for(:base).join(", "), class: "alert alert-alert")
+  def show_errors_on_base(resource)
+    if resource.errors.any? && resource.errors.messages[:base].present?
+      content_tag(:div, resource.errors.full_messages_for(:base).join(", "), class: "alert alert-alert")
     end
   end
 
-  def show_errors_inline(obj, field_name)
-    if obj.errors.any? && obj.errors.full_messages_for(field_name).present? && !obj.errors[:base].present?
-      content_tag(:div, obj.errors.full_messages_for(field_name).join(", "), class: "alert alert-alert inline-alert")
+  def show_errors_inline(resource, field_name)
+    if resource.errors.any? && resource.errors.full_messages_for(field_name).present? && !resource.errors[:base].present?
+      content_tag(:div, resource.errors.full_messages_for(field_name).join(", "), class: "alert alert-alert inline-alert")
     end
   end
 end
