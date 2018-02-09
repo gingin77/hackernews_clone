21380b21dafa8c24a8065caeff6fa906f18a4619
gingin77 <ginniehench@gmail.com>
2018-02-08 22:19:51 -0500

    Update seeds file to use Post and Comment in place of Submission

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,44 +4,51 @@ if User.first.nil?
     email: 'ginniehench@gmail.com',
     password: 'secret!'
   })
+
   user2 = User.create({
     name: 'Ellie',
     email: 'ellie@awesomedogs.com',
     password: 'secret!'
   })
-  posts = Submission.create([
+
+  posts = Post.create([
     {
-      user: user1,
+      submitter: user1,
       text: "Plants don’t get enough credit. They move. You know this. Your houseplant salutes the sun each morning. At night, it returns to center."
     },
     {
-      user: user2,
+      submitter: user2,
       url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience",
       title: "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?"
     }
   ])
-  Submission.create({
-    user: user1,
+
+  Comment.create({
+    submitter: user1,
     post: posts[0],
     text: "Yay for plants..."
   })
+
 elsif (!User.first.nil? && !User.last.nil?)
   ar_user1 = User.first
   ar_user2 = User.last
-  posts = Submission.create([
+
+  posts = Post.create([
     {
-      user: ar_user1,
+      submitter: ar_user1,
       text: "Plants don’t get enough credit. They move. You know this. Your houseplant salutes the sun each morning. At night, it returns to center."
     },
     {
-      user: ar_user2,
+      submitter: ar_user2,
       url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience",
       title: "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?"
     }
   ])
-  Submission.create({
-    user: ar_user1,
+
+  Comment.create({
+    submitter: ar_user1,
     post: posts[0],
     text: "Yay for plants..."
   })
+
 end
