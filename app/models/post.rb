9004ce8095f320cb59a523a02f53e7345ffdd73a
gingin77 <ginniehench@gmail.com>
2018-02-08 21:53:08 -0500

    Write more specs and validations for Post model - all existing specs pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,32 @@ class Post < ApplicationRecord
 
   belongs_to :submitter, class_name: :User, foreign_key: :user_id
   has_many :comments
+
+  validates :title, :url, presence: true, if: :url_post?
+  validates :title, :url, absence: true, if: :text_post?
+
+  validate :at_least_one_field_present
+  validate :a_post_can_only_be_one_type
+
+  def url_post?
+    url.present? || title.present?
+  end
+
+  def text_post?
+    text.present?
+  end
+
+  private
+
+  def a_post_can_only_be_one_type
+    if text_post? && url_post?
+      errors.add(:base, "You may submit either text content OR a url link, NOT both")
+    end
+  end
+
+  def at_least_one_field_present
+    if text.blank? && url.blank? && title.blank?
+      errors.add(:base, "You must submit a url link OR text content")
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
-RSpec.describe Post, :type => :model do
-  it "creates a text post" do
-    post = Post.new( text: "I'm a new text post", submitter: create(:user_a) )
+RSpec.describe Post, type: :model do
+  it "creates a post by a submitter" do
+    post = Post.new(text: "I'm a new text post", submitter: create(:user_a))
     post.save
 
     expect(post.persisted?).to eq(true)
@@ -15,7 +15,7 @@ RSpec.describe Post, :type => :model do
     expect(post.persisted?).to eq(false)
   end
 
-  it "creates a url post" do
+  it "creates a url post with a title by a submitter" do
     post = Post.new(
       url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia.html?rref=collection%2Fsectioncollection%2Fscience",
       title: "Sedate a Plant, and It Seems to Lose Consciousness. Is It Conscious?",
@@ -25,18 +25,41 @@ RSpec.describe Post, :type => :model do
     expect(post.persisted?).to eq(true)
   end
 
+  it "finds a url post without a url to be invalid" do
+    post = build(:url_post, url: nil)
+    post.save
+
+    expect(post.persisted?).to eq(false)
+  end
+
+  it "finds a url post without a title to be invalid" do
+    post = build(:url_post, title: nil)
+    post.save
+
+    expect(post.persisted?).to eq(false)
+  end
+
   it "finds a text post with a url to be invalid" do
     post = build(:text_post, url: "https://www.nytimes.com/2018/02/02/science/plants-consciousness-anesthesia")
     post.save
 
-    expect(post.persisted?).to eq(true)
+    expect(post.persisted?).to eq(false)
+    expect(post.errors[:base]).to include("You may submit either text content OR a url link, NOT both")
   end
 
   it "finds a text post with a title to be invalid" do
     post = build(:text_post, title: "A title")
     post.save
 
-    expect(post.persisted?).to eq(true)
+    expect(post.persisted?).to eq(false)
+    expect(post.errors[:base]).to include("You may submit either text content OR a url link, NOT both")
   end
 
+  it "finds a post without any inputs to be invalid" do
+    post = Post.new(title: nil, url: nil, text: nil, submitter: create(:user_a))
+    post.save
+
+    expect(post.persisted?).to eq(false)
+    expect(post.errors[:base]).to include("You must submit a url link OR text content")
+  end
 end
