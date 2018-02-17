1759663e8646633c4acf302045adff82000c3df4
gingin77 <ginniehench@gmail.com>
2018-02-16 22:59:51 -0500

    Add more specs for Vote model and more validations.

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,17 @@
 class Vote < ApplicationRecord
-  belongs_to :voter, class_name: :User, foreign_key: :user_id
-  belongs_to :voteable, polymorphic: true
+  belongs_to :voter,
+             class_name: :User,
+             foreign_key: :user_id
 
-  validates_uniqueness_of :user_id, scope: [:voteable_id, :voteable_type] 
+  belongs_to :voteable,
+             polymorphic: true
+
+  validates_uniqueness_of :user_id,
+             scope: [:voteable_id, :voteable_type]
+
+  validates_numericality_of :value, {
+              only_integer: true,
+              greater_than_or_equal_to: -1,
+              less_than_or_equal_to: 1
+            }
 end
--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -2,10 +2,17 @@ require "rails_helper"
 
 RSpec.describe Vote, type: :model do
   subject { create(:vote) }
-  
-  it { should belong_to(:voteable) }
-  it { should belong_to(:voter) }
-  it { should have_attribute(:value) }
 
-  it { should validate_uniqueness_of(:user_id).scoped_to(:voteable_id, :voteable_type) }
+  it { is_expected.to belong_to(:voteable) }
+  it { is_expected.to belong_to(:voter) }
+  it { is_expected.to have_attribute(:value) }
+
+  it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:voteable_id, :voteable_type) }
+  it { is_expected.to validate_numericality_of(:value) }
+  it { is_expected.to allow_value(1).for(:value) }
+  it { is_expected.to allow_value(-1).for(:value) }
+  it { is_expected.not_to allow_value(2).for(:value) }
+  it { is_expected.not_to allow_value(-2).for(:value) }
+  it { is_expected.not_to allow_value(0.9999).for(:value) }
+  it { is_expected.not_to allow_value(0.9999).for(:value) }
 end
