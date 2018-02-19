6f0182a997551fe325462325e2bda3c574a09161
gingin77 <ginniehench@gmail.com>
2018-02-19 11:11:50 -0500

    Ingegration test for vote buttons is working
    At present, when a logged in user goes to the /posts/ page, they will see orange up and down vote buttons and a grey x next to posts they have NOT voted on. Once they vote, the greyed out ex switches to orange and is clickable... Next, need to refactor conditional on main index page to evaluate if a vote exists

--- spec/features/vote_on_posts_spec.rb
+++ spec/features/vote_on_posts_spec.rb
@@ -4,15 +4,15 @@ feature "Vote on a post" do
   let(:voter) { create(:oliver) }
   let(:url_post) { create(:url_post) }
   let(:text_post) { create(:text_post) }
-  let(:up_vote) do
-    create :vote, {
-      value: 1,
-      voter: voter,
-      voteable: url_post
-    }
-  end
+  # let(:up_vote) do
+  #   create :vote, {
+  #     value: 1,
+  #     voter: voter,
+  #     voteable: url_post
+  #   }
+  # end
 
-  it "allows authenticated user to vote on a post" do
+  it "allows authenticated user to vote on a post once" do
     sign_in voter
     visit post_path(url_post)
     expect(page).to have_content url_post.title
@@ -20,14 +20,16 @@ feature "Vote on a post" do
     visit posts_path
     expect(page).to have_content url_post.title
 
-    expect(page).to have_css "button#up"
-    expect(page).to have_css "button#down"
-    expect(page).to have_css "a#delete"
+    expect(page).to have_button("up", class: "clickable")
+    expect(page).to have_button("down", class: "clickable")
+    expect(page).to have_css "i.not-clickable, i.fa-times-circle"
 
-    expect(page).to have_link("", :href=>"votes/#{up_vote.id}")
-    # expect(page).to have_no_link("Foo", :href=>"google.com")
+    click_button "up"
 
-    # click_on "up-vote"
+    expect(page).to have_button("up", class: "not-clickable")
+    expect(page).to have_button("down", class: "clickable")
+    expect(page).to have_link("delete", href: "/votes/#{Vote.last.id}")
+    expect(page).to have_css "i.clickable, i.fa-times-circle"
   end
 
   it "blocks a user who has voted from casting same vote again" do
