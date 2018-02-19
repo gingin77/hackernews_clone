dd9a0b53983b60facab2280c6697bf9840478047
gingin77 <ginniehench@gmail.com>
2018-02-19 18:31:12 -0500

    Base Vote Controller redirects on referrer path. See private method.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,25 +1,24 @@
 class VotesController < ApplicationController
   before_action :authenticate_user!
+  before_action :request_referrer
 
   helper_method :vote
 
   def create
     vote = current_user.votes.build(vote_params)
     vote.save
-    # redirect_to polymorphic_url(vote.voteable_type.constantize) << goes to posts#index
-    redirect_to polymorphic_url(vote.voteable)
-    # ^^ goes to post#show
+    redirect_to request_referrer
   end
 
   def update
     vote.value == 1 ? vote.value = -1 : vote.value = 1
     vote.save
-    redirect_to polymorphic_url(vote.voteable_type.constantize)
+    redirect_to request_referrer
   end
 
   def destroy
     vote.destroy
-    redirect_to polymorphic_url(vote.voteable_type.constantize)
+    redirect_to request_referrer
   end
 
   private
@@ -30,5 +29,14 @@ class VotesController < ApplicationController
 
   def vote_params
     params.require(:vote).permit(:voteable_type, :voteable_id, :value)
+  end
+
+  def request_referrer
+    referrer = Rails.application.routes.recognize_path(request.referrer)
+    if "index" == referrer[:action]
+      "/#{referrer[:controller]}"
+    else
+      "/#{referrer[:controller]}/#{referrer[:id]}"
+    end
   end
 end
