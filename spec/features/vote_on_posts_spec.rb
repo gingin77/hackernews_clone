4190199665b991e1c0d08f1e4f3d2514904a01df
gingin77 <ginniehench@gmail.com>
2018-02-19 12:47:48 -0500

    Enhance vote button appearance and functionality
    Integration tests for adding an up vote and deleting a vote have been written and are passing
    The value of a vote is evaluated in the posts/index view - if +1, the up arrow is greyed out and not attached to a form and vice versa
    Next - look for ways to DRY up the code; Add edit action in controller and replace new_vote form with edit_vote form

--- spec/features/vote_on_posts_spec.rb
+++ spec/features/vote_on_posts_spec.rb
@@ -2,46 +2,51 @@ require "rails_helper"
 
 feature "Vote on a post" do
   let(:voter) { create(:oliver) }
-  let(:url_post) { create(:url_post) }
-  let(:text_post) { create(:text_post) }
-  # let(:up_vote) do
-  #   create :vote, {
-  #     value: 1,
-  #     voter: voter,
-  #     voteable: url_post
-  #   }
-  # end
+  let!(:url_post) { create(:url_post) }
 
-  it "allows authenticated user to vote on a post once" do
+  before :each do
     sign_in voter
-    visit post_path(url_post)
-    expect(page).to have_content url_post.title
-
     visit posts_path
     expect(page).to have_content url_post.title
+  end
 
+  def posts_vote_count
+    find("p span").text.to_i
+  end
+
+  def cast_up_vote
+    click_button "up"
+  end
+
+  def cancel_vote
+    click_link "delete"
+  end
+
+  it "allows authenticated user to vote up on a post once" do
+    expect(posts_vote_count).to eq(0)
     expect(page).to have_button("up", class: "clickable")
     expect(page).to have_button("down", class: "clickable")
     expect(page).to have_css "i.not-clickable, i.fa-times-circle"
 
-    click_button "up"
+    cast_up_vote
 
-    expect(page).to have_button("up", class: "not-clickable")
+    expect(posts_vote_count).to eq(1)
+    expect(page).to have_css "i.not-clickable, i.fa-chevron-circle-up"
     expect(page).to have_button("down", class: "clickable")
-    expect(page).to have_link("delete", href: "/votes/#{Vote.last.id}")
+
     expect(page).to have_css "i.clickable, i.fa-times-circle"
+    expect(page).to have_link("delete", href: "/votes/#{Vote.last.id}")
   end
 
-  it "blocks a user who has voted from casting same vote again" do
-    skip
-    sign_in voter
-    visit post_path(url_post)
-    expect(page).to have_content url_post.title
+  it "allows voter to cancel their vote" do
+    cast_up_vote
+    expect(posts_vote_count).to eq(1)
 
-    visit posts_path
-    expect(page).to have_content url_post.title
+    cancel_vote
+    expect(posts_vote_count).to eq(0)
+    expect(page).to have_button("up", class: "clickable")
+    expect(page).to have_button("down", class: "clickable")
 
-    expect(page).to have_css "button.my-button"
-    # click_on "up-vote"
+    expect(page).to have_css "i.not-clickable, i.fa-times-circle"
   end
 end
