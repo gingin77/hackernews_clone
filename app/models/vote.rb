41f149de17ad62f30f6fa2746e009124a025a395
gingin77 <ginniehench@gmail.com>
2018-02-20 10:29:20 -0500

    Adjust indentations and spacing in Vote model and in spec for vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,17 +1,17 @@
 class Vote < ApplicationRecord
   belongs_to :voter,
-              class_name: :User,
-              foreign_key: :user_id
+             class_name:  :User,
+             foreign_key: :user_id
 
   belongs_to :voteable,
               polymorphic: true
 
   validates_uniqueness_of :user_id,
-              scope: [:voteable_id, :voteable_type]
+                          scope: [:voteable_id, :voteable_type]
 
   validates_numericality_of :value, {
-              only_integer: true,
-              greater_than_or_equal_to: -1,
-              less_than_or_equal_to: 1
-            }
+                                      only_integer:             true,
+                                      greater_than_or_equal_to: -1,
+                                      less_than_or_equal_to:    1
+                                    }
 end
--- spec/models/voter_spec.rb
+++ spec/models/voter_spec.rb
@@ -3,11 +3,15 @@ require "rails_helper"
 RSpec.describe Vote, type: :model do
   subject { create(:vote) }
 
-  it { is_expected.to belong_to(:voteable) }
-  it { is_expected.to belong_to(:voter) }
   it { is_expected.to have_attribute(:value) }
 
-  it { is_expected.to validate_uniqueness_of(:user_id).scoped_to(:voteable_id, :voteable_type) }
+  it { is_expected.to belong_to(:voteable) }
+  it { is_expected.to belong_to(:voter) }
+
+  it do
+    is_expected.to validate_uniqueness_of(:user_id)
+      .scoped_to(:voteable_id, :voteable_type)
+  end
   it { is_expected.to validate_numericality_of(:value) }
   it { is_expected.to allow_value(1).for(:value) }
   it { is_expected.to allow_value(-1).for(:value) }
