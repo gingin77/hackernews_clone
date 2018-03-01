52b5e6c3eebd083e9fa323194206308b36cc7237
gingin77 <ginniehench@gmail.com>
2018-03-01 07:57:17 -0500

    Reorganize vote/_presenter_index and refactor VoteButtonPresenter more

--- app/presenters/vote_button_presenter.rb
+++ app/presenters/vote_button_presenter.rb
@@ -5,43 +5,67 @@ class VoteButtonPresenter < ApplicationPresenter
     @vote ||= Vote.find_by(user_id: h.current_user, voteable_id: parent)
   end
 
-  def up
-    if vote&.up_vote?
-      up_icon
-    elsif vote&.down_vote? || vote.blank?
-      h.render partial: "shared/vote/up", locals: { parent: parent }
-    end
-  end
-
-  def down
-    if vote&.down_vote?
-      down_icon
-    elsif vote&.up_vote?  || vote.blank?
-      h.render partial: "shared/vote/down", locals: { parent: parent }
-    end
-  end
-
-  def cancel
-    if h.user_signed_in? && vote&.present?
-      h.render partial: "shared/vote/cancel", locals: { vote: vote }
+  def buttons
+    if !h.user_signed_in?
+      static_icons
     else
-      cancel_icon
+      if vote.blank?
+        new_vote_sequence
+      elsif vote&.up_vote?
+        up_vote_sequence
+      elsif vote&.down_vote?
+        down_vote_sequence
+      end
     end
-  end
-
-  def up_icon
-    h.content_tag :i, "", class: "fa fa-chevron-circle-up not-clickable"
-  end
-
-  def down_icon
-    h.content_tag :i, "", class: "fa fa-chevron-circle-down not-clickable"
-  end
-
-  def cancel_icon
-    h.content_tag :i, "", class: "fa fa-times-circle not-clickable"
   end
 
   def static_icons
-    h.render partial: "shared/vote/static_icons"
+    render_partial("static_icons", nil)
+  end
+
+  def new_vote_sequence
+    active_up_button + active_down_button + cancel_icon
+  end
+
+  def up_vote_sequence
+    up_icon + active_down_button + active_delete_button
+  end
+
+  def down_vote_sequence
+    active_up_button + down_icon + active_delete_button
+  end
+
+  def active_up_button
+    render_partial("up", { parent: parent })
+  end
+
+  def active_down_button
+    render_partial("down", { parent: parent })
+  end
+
+  def active_delete_button
+    render_partial("cancel", { vote: vote })
+  end
+
+  def up_icon
+    icon_temp("chevron-circle-up")
+  end
+
+  def down_icon
+    icon_temp("chevron-circle-down")
+  end
+
+  def cancel_icon
+    icon_temp("times-circle")
+  end
+
+  private
+
+  def render_partial(name, locals)
+    h.render partial: ["shared/vote", name].join('/'), locals: locals
+  end
+
+  def icon_temp(icon_type)
+    h.content_tag :i, "", class: "fa fa-#{icon_type} not-clickable"
   end
 end
