6b0a446cadecceabae8ff30f9cc49ffa82e89bc3
gingin77 <ginniehench@gmail.com>
2018-05-22 12:12:51 -0400

    Add a post controller spec to cover create action

---
+++ spec/controllers/posts_spec.rb
@@ -0,0 +1,32 @@
+require "rails_helper"
+
+RSpec.describe PostsController do
+  describe "POST create" do
+    let!(:user) { create(:user, :oliver) }
+
+    context "as logged in user" do
+      before do
+        sign_in user
+      end
+
+      it "allows a logged in user to create a url post" do
+        expect {
+          post :create, params: { post: {
+            title: "Can you believe this??!?!",
+            url: "http://www.latimes.com/politics/la-na-pol-essential-washington-updates-epa-bars-ap-cnn-from-summit-on-1527003250-htmlstory.html"} }
+        }.to change {
+          Post.count
+        }.by(1)
+      end
+
+      it "allows a logged in user to create a text post" do
+        expect {
+          post :create, params: { post: {
+            text: "I wish these weren't the people in charge...."} }
+        }.to change {
+          Post.count
+        }.by(1)
+      end
+    end
+  end
+end
