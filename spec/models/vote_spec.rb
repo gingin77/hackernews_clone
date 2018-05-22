63d7a944fc7f39640261f6ccca90f8931decf409
gingin77 <ginniehench@gmail.com>
2018-05-22 11:31:28 -0400

    Correct typo in vote model spec filename; add tests for added methods

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -24,4 +24,29 @@ RSpec.describe Vote, type: :model do
   it { is_expected.not_to allow_value(-2).for(:value) }
   it { is_expected.not_to allow_value(0.9999).for(:value) }
   it { is_expected.not_to allow_value(-0.9999).for(:value) }
+
+  describe "added methods" do
+    let!(:up_vote)   { create(:up_vote) }
+    let!(:down_vote) { create(:down_vote) }
+
+    describe "#vote_type" do
+      it "returns 'up' when value is positiive 1" do
+        expect(up_vote.vote_type).to eq("up")
+      end
+
+      it "returns 'down' when value is negative 1" do
+        expect(down_vote.vote_type).to eq("down")
+      end
+    end
+
+    describe "#up_vote?" do
+      it { expect(up_vote.up_vote?).to   eq(true) }
+      it { expect(down_vote.up_vote?).to eq(false) }
+    end
+
+    describe "#down_vote?" do
+      it { expect(down_vote.down_vote?).to eq(true) }
+      it { expect(up_vote.down_vote?).to   eq(false) }
+    end
+  end
 end
