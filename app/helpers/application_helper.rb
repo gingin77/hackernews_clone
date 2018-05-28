2ff76160c6dfda42128cfd21526c5e030d6bd761
gingin77 <ginniehench@gmail.com>
2018-05-28 14:43:16 -0400

    Change local var name in present method; remove new lines from end of js.erb files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,10 +11,9 @@ module ApplicationHelper
     end
   end
 
-  def present(model, klass = nil)
-    klass ||= "#{model.class}Presenter".constantize
-    presenter = klass.new(model, self)
+  def present(object, klass = nil)
+    klass ||= "#{object.class}Presenter".constantize
+    presenter = klass.new(object, self)
     yield presenter if block_given?
-    presenter
   end
 end
